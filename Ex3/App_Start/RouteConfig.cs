72c9355d572fec3de05a89dbdefd6a0785316f9a
Lahav <Lahav016@gmail.com>
2019-05-28 15:09:09 +0300

    Added routing.

---
+++ Ex3/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ex3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Option1", "display/{ip}/{port}", 
+                defaults: new {controller = "Data", action = "Option1"});
+
+            routes.MapRoute("Option2", "display/{ip}/{port}/{samples}",
+                defaults: new { controller = "Data", action = "Option2"});
+        }
+    }
+}
---
+++ Ex3/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex3.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public ActionResult Option1(string ip, int port)
+        {
+            //
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Option2(string ip, int port, int samples)
+        {
+            //
+            return View();
+        }
+    }
+}
